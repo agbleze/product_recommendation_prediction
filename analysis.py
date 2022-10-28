a5ff0455d55688c4ec594531b5e731d697e00b38
Agbleze <lagbleze@st.ug.edu.gh>
2022-10-28 02:00:08 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,11 +4,50 @@ import os
 import pandas as pd
 
 
+# %%
+cwd_path = os.getcwd()
 
 
+# %%
+data_path = cwd_path + '/data/product_reviews.csv'
+
+
+df = pd.read_csv(data_path)
+
+
+#%%
+
+df.columns
+
+#%%
+review_df =  df[['reviews.text', 'reviews.doRecommend']]
+
+#%%
+review_df['reviews.doRecommend'].value_counts()
+
+
+#%%
+review_df.dropna()
+
+
+#%%
+review_df_clean = review_df[review_df['reviews.text']!='Rating provided by a verified purchaser'].dropna()
 
 
 
 # %%
-cwd_path = os.getcwd()
+review_df_clean['reviews.doRecommend'].value_counts()
+
+#%%
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+
+
+
+
+
 # %%
