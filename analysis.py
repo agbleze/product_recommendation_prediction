7a12ad868bc3702d3a71cd11d7c9fbf8e5641063
Agbleze <lagbleze@st.ug.edu.gh>
2022-10-28 02:14:44 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -42,6 +42,10 @@ review_df_clean['reviews.doRecommend'].value_counts()
 from sklearn.model_selection import train_test_split
 
 
+#%%
+
+train_test_split(review_df_clean, train_size=0.7, stratify='reviews.doRecommend',
+                 random_state=0)
 
 
 
