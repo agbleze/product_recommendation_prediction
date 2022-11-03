0f147bd321edec99aa027b996cb7a228a3c1847e
Agbleze <lagbleze@st.ug.edu.gh>
2022-11-03 22:29:07 +0100

    Update Vocabulary.py

--- Vocabulary.py
+++ Vocabulary.py
@@ -8,7 +8,7 @@ class ReviewVocabulary(object):
         if token_to_idx is None:
             self._token_to_idx = token_to_idx
             
-        idx_to_token = {idx: token for token, idx in token_to_idx}
+        self._idx_to_token = {idx: token for token, idx in token_to_idx}
         
         
     def to_serializable(self):
@@ -17,6 +17,16 @@ class ReviewVocabulary(object):
     @classmethod
     def from_serializable(cls, contents):
         return cls(**contents)
+    
+    def add_token(self, token):
+        if token in self._token_to_idx:
+            idx = self._token_to_idx[token]
+        else:
+            idx = len(self._token_to_idx)
+            self._token_to_idx[token] = idx
+            self._idx_to_token[idx] = token
+        return idx
+            
     
     
 class ReviewSequenceVocabulary(ReviewVocabulary):
