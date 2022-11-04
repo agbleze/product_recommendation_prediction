76d0236ddab5492dfdf2405855d954eedb98c6b2
Agbleze <lagbleze@st.ug.edu.gh>
2022-11-04 23:31:43 +0100

    Update Vocabulary.py

--- Vocabulary.py
+++ Vocabulary.py
@@ -90,12 +90,23 @@ class ReviewVectorizer:
         self.review_vocab = ReviewSequenceVocabulary()
         self.recommend_vocab = ReviewSequenceVocabulary()
         
-    def vectorize(self, review_vocab, vector_length = -1):
-        indices = [review_vocab.begin_seq_index]
-        indices = indices.extend(self.review_vocab.add_token(token) for token in review_vocab._token_to_idx)
-        indices = indices.append(review_vocab.mask_index)
+    def vectorize(self, vector_length = -1):
+        indices = [self.review_vocab.begin_seq_index]
+        indices = indices.extend(self.review_vocab.add_token(token) for token in self.review_vocab._token_to_idx)
+        indices = indices.append(self.review_vocab.mask_index)
         
-        outer_vector = np
+        if vector_length >= 0:
+            outer_vector = np.empty(len(indices), dtype=np.int64)
+            outer_vector[:len(indices)] = indices
+            outer_vector[len(indices):] = self.review_vocab.mask_index
+            return outer_vector
+        
+    def get_vectorizer(self):
+        return self.vectorize(vector_length=-1)
+    
+    
+    def from_dataframe(self):
+        pass
         
         
         
