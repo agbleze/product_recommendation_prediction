8e56936ca0258f69aa91b369fd83e52de4923e61
Agbleze <lagbleze@st.ug.edu.gh>
2022-11-06 13:22:17 +0100

    Update ReviewVectorizer.py

--- ReviewVectorizer.py
+++ ReviewVectorizer.py
@@ -1,35 +1,43 @@
+#%%
 from Vocabulary import ReviewSequenceVocabulary, ReviewVocabulary
 import numpy as np
 import pandas as pd
 import string
 
-def preprocess_text(sentence):
+#%%
+def preprocess_texts_to_tokens(sentence):
+    text_tokens = []
     for text in sentence.split(" "):
             text = text.lower()
             if text not in string.punctuation and not text.isnumeric():
                 if text[-1] in string.punctuation:
                     text = text[:-1]
-                return text
+                text_tokens.append(text)
+    return text_tokens
     
+sent= "this is a Great proDUCT?"
 
+
+for token in preprocess_texts_to_tokens(sent):
+    print(token)
+
+#print(res)
+
+
+#print(sent_list)
+
+#%%
 class ReviewVectorizer:
     def __init__(self, review_df):
         self.review_df = review_df
         
         self.review_vocab = ReviewSequenceVocabulary()
-        self.recommend_vocab = ReviewSequenceVocabulary()
+        self.recommend_vocab = ReviewVocabulary()
         
     def vectorize(self, review_text, vector_length = -1):
-        text_list = []
-        for text in review_text.split(" "):
-            text = text.lower()
-            if text not in string.punctuation and not text.isnumeric():
-                if text[-1] in string.punctuation:
-                    text = text[:-1] 
-                text_list.append(text)
         
         indices = [self.review_vocab.begin_seq_index]
-        indices = indices.extend(self.review_vocab.lookup_token(token) for token in self.review_vocab._token_to_idx)
+        indices = indices.extend(self.review_vocab.lookup_token(token) for token in preprocess_texts_to_tokens(review_text))
         indices = indices.append(self.review_vocab.mask_index)
         
         if vector_length >= 0:
@@ -42,12 +50,14 @@ class ReviewVectorizer:
         return self.vectorize(vector_length=-1)
     
     
+    #@classmethod
     def from_dataframe(self):
+        #cls.review_df = review_df
         for row, text_column in self.review_df.iterrows():
-            for text in text_column['reviews.text'].split(" "):
-                self.review_vocab_token = self.self.review_vocab.add_token(text) 
+            for token in preprocess_texts_to_tokens(text_column['reviews.text']):
+                self.review_vocab_token = self.self.review_vocab.add_token(token) 
         
-        for token in set(self.review_df['reviews.doRecommend'].split(" ")):
+        for token in sorted(set(preprocess_texts_to_tokens(self.review_df['reviews.doRecommend']))):
             self.recommend_vocab.add_token(token)
             
         return (self.review_vocab_token, self.recommend_vocab)
