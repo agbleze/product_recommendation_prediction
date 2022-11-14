af2acf70773ff3cfe759d34b50dd105e160f7993
Agbleze <lagbleze@st.ug.edu.gh>
2022-11-14 21:27:03 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,9 +1,34 @@
 from argparse import Namespace
 import os
+import torch
 
 
 args = Namespace(
     review_csv = "",
-    vectorizer_filepath = ""
+    vectorizer_filepath = "",
+    model_state_file = "",
+    save_dir = "",
+    glove_filepath = "",
+    use_glove=False,
+    embedding_size=100,
+    hidden_dim=100,
+    num_channels=100,
+    
+    seed=1337,
+    learning_rate=0.001,
+    dropout_p=0.1,
+    batch_size=128,
+    num_epochs=5,
+    early_stopping_criteria=5,
+    
+    cuda = True,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepath_to_save_dir=True
 )
+
+
+if not torch.cuda.isavailable:
+    cuda = False
+
 
