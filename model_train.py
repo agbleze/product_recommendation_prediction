4c1803b1ce5294849023e0c94d8dc30e1c870a22
Agbleze <lagbleze@st.ug.edu.gh>
2022-11-15 23:32:50 +0100

    Create model_train.py

---
+++ model_train.py
@@ -0,0 +1,21 @@
+from helpers import (Namespace,
+                     load_glove_from_file,
+                     set_seed_everywhere,
+                     handle_dirs,make_embedding_matrix,
+                     make_train_state,update_train_state,
+                     compute_accuracy
+                     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
